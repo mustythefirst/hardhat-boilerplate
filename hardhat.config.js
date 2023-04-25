c42bfcd47de01983680a25baa33bef4d3d90ad1a
mustythefirst <m.adde.osman@gmail.com>
2023-04-25 04:38:47 +0100

    https://github.com/mustythefirst/hardhatboilerplate.git

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+
+
+const ALCHEMY_API_KEY = process.env.API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
